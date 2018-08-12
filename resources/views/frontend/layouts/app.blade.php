f7f0208aefa2e5a9bbd9b257ba1639c59d232c05
tuan.ca <tuan.ca@neo-lab.vn>
2018-08-12 12:03:03 +0700

    add online

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -224,17 +224,19 @@
 <script src="{{ asset('js/admin/common.js') }}"></script>
 @stack('scripts')
 <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/59ef3f0a4854b82732ff7637/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5b6fbd21afc2c34e96e77ed3/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  })();
 </script>
+<!--End of Tawk.to Script-->
 <!--End of Tawk.to Script-->
 @stack('scripts')
 </body>
